955c97474def0cffe90973c842e1df65e61de5d4
Yasin <yasin@gencpro.com>
2020-06-26 10:08:13 +0300

    fon takip reactJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+// comps
+import Header from './components/Header'
+import Portfolio from "./components/Portfolio";
+
+library.add(fas);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid m-0 p-0">
+      <Header/>
+      <Portfolio/>
     </div>
   );
 }
